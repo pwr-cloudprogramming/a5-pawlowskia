56b9a61ce088a0148d2bb09680b3a3f95583b257
adam <apawlowski@fream.pl>
2024-05-06 00:28:24 +0200

    correct ipfinder script

--- ipfinder.sh
+++ ipfinder.sh
@@ -8,4 +8,4 @@ INTERFACE=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/ | h
 SUBNET_ID=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/${INTERFACE}/subnet-id`
 VPC_ID=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/network/interfaces/macs/${INTERFACE}/vpc-id`
 echo "Your EC2 instance works in :AvailabilityZone: ${AZONE} / VPC: ${VPC_ID} / VPC subnet: ${SUBNET_ID} / IP address: ${IP_V4}"
-echo 'const socket = io("ws://${IP_V4}:3000");' | cat - ./frontend/js/socket.js > temp && mv temp ./frontend/js/socket.js
+echo 'const socket = io("ws://'${IP_V4}':3000");' | cat - ./frontend/js/socket.js > temp && mv temp ./frontend/js/socket.js
